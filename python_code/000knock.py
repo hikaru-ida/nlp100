f505aa15bb852474c7bceea6bb77fb2b52948f0d
monk <troublemonk11@gmail.com>
2016-03-02 04:43:29 +0900

    add 001knock.py and 002knock.py

--- python_code/000knock.py
+++ python_code/000knock.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#自分のコード
+sentence = "stressed"
+str1 = ""
+for i in range(len(sentence)):
+    str1 += sentence[len(sentence) - i - 1]
+print str1
 
-print "abc"
+#他の人のやつ
+print sentence[::-1]
+
+
+
+
---
+++ python_code/002knock.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#自分のやつ（一つ目）
+str1 = u"パトカー"
+str2 = u"タクシー"
+str3 = u""
+for i in range(len(str1)):
+    str3 += str1[i] + str2[i]
+
+print str3.encode('utf-8')
+
+#他の人のやつ
+s = ""
+for i, j in zip(str1, str2):
+    s+= i + j
+
+print s.encode('utf-8')
