6dbbe7c1dd886bc76bdc210a54c9eea0551f5c55
hikaru <iidhkr@gmail.com>
2017-01-09 22:16:15 +0900

    modified knock005.py

--- python_code/knock005.py
+++ python_code/knock005.py
@@ -2,10 +2,18 @@ def seqToWordNgram(n, seq):
     seq = seq.replace(",", "")
     seq = seq.replace(".", "")
     ls = seq.split()
-    ngram_ls = []
+    word_ngram_ls = []
     for i in range(len(ls)-n+1):
-        ngram_ls.append(" ".join(ls[i:i+n]))
-    return ngram_ls
+        word_ngram_ls.append(" ".join(ls[i:i+n]))
+    return word_ngram_ls
+
+
+def seqToCharNgram(n, seq):
+    char_ngram_ls = []
+    for i in range(len(seq)-n+1):
+        char_ngram_ls.append(seq[i:i+n])
+    return char_ngram_ls
 
 s = "I am an NLPer"
 print(seqToWordNgram(2, s))
+print(seqToCharNgram(2, s))
